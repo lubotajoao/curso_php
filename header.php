7e385c0d4c79cbfd460c2f7ede2078cdb5093a90
Lubota Joao <lubota4@gmail.com>
2019-01-15 15:47:10 -0200

    Curso PHP Basico - [Numeros]

--- header.php
+++ header.php
@@ -2,14 +2,12 @@
 
 <?php include 'functions/functions.php'; ?>
 
-<html>
+<html lang="pt-br">
 
-	<head>
-		<title>Curso de PHP - Ivan Lourenço Gomes</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style.css"></link>
-		<link rel="stylesheet" type="text/css" href="css/gallery-001.css"></link>
-		
-
-
-	</head>
+<head>
+	<title>Curso de PHP - Ivan Lourenço Gomes</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<!--	<link rel="stylesheet" type="text/css" href="css/gallery-001.css"/>-->
+</head>
+</html>
