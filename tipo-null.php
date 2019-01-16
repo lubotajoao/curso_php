51a42442046f557b0f263e8d9aae9156dc74f368
Lubota Joao <lubota4@gmail.com>
2019-01-16 02:17:48 -0200

    Curso PHP Basico - [Tipo NULL]

--- tipo-null.php
+++ tipo-null.php
@@ -1,38 +1,40 @@
-	<?php 
-		include 'header.php';
-		include 'functions/functions.php';
-		$aula_atual = 'tipo-null';
-	?>
+<?php
+include 'header.php';
+include 'functions/functions.php';
+$aula_atual = 'tipo-null';
+?>
+
+<body>
+
+<h2>NULL</h2>
+<hr>
+<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+
+<h3>NULL = Ausência de valor</h3>
+
+<?php
+$posts = '';
+var_dump($posts);
+
+echo "<br><br>";
+
+if ($posts == NULL) {
+	echo "A variavel e' igual a NULL";
+} else {
+	echo "A variavel nao e' igual a NULL";
+}
+
+?>
+
+<h3>Resultado da pesquisa</h3>
+
+<h3>Agora é a sua vez</h3>
+
+<p>Crie um Array, de preferência multidimensional, e solte as informações de cada item deste array em uma lista usando o
+	loop foreach.</p>
+<br>
 
 
-	<body>
+<?php include 'functions/bottom_index.php'; ?>
 
-
-		<h2>NULL</h2>
-		<hr>
-		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
-
-		<h3>NULL = Ausência de valor</h3>
-
-
-		<h3>Resultado da pesquisa</h3>
-
-		<h3>Agora é a sua vez</h3>
-
-			<p>Crie um Array, de preferência multidimensional, e solte as informações de cada item deste array em uma lista usando o loop foreach.</p>
-			<br>
-
-		
-
-
-
-
-
-
-
-		<?php include 'functions/bottom_index.php'; ?>
-
-
-	</body>
-
-</html>
+</body>
