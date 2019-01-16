d681d4d635a67b4b123f9ca86c834381afd371ae
Lubota Joao <lubota4@gmail.com>
2019-01-16 00:30:09 -0200

    Curso PHP Basico - [Booleans]

--- booleans.php
+++ booleans.php
@@ -1,29 +1,23 @@
-<?php 
-		include 'header.php';
-		$aula_atual = 'booleans';
-	?>
+<?php
+include 'header.php';
+$aula_atual = 'booleans';
+?>
 
 
-	<body>
+<body>
 
-		<h2>BOOLEANS</h2>
-		<hr>
-		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+<h2>BOOLEANS</h2>
+<hr>
+<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
 
-		<h3>True ou False?</h3><br>
-		
-		<p></p>
+<h3>True ou False?</h3><br>
 
+<?php $teste = 30 > 20;?>
 
-
-
+<p><?php var_dump($teste);?></p>
 
 
+<?php include 'functions/bottom_index.php'; ?>
 
 
-		<?php include 'functions/bottom_index.php'; ?>
-
-
-	</body>
-
-</html>
+</body>
--- include-require.php
+++ include-require.php
@@ -1,79 +1,54 @@
-<?php 
-		include 'header.php';
-		$aula_atual = 'include-require';
-	?>
+<?php
+include 'header.php';
+$aula_atual = 'include-require';
+?>
 
 
-	<body>
+<body>
 
 
-		<h2>Include e Require</h2>
-		<hr>
-		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+<h2>Include e Require</h2>
+<hr>
+<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
 
 
-		
+<h3>Include</h3>
 
-		<h3>Include</h3>
-		
-		<?php 
+<?php
 
-		?>
+?>
 
 
-		<p><?php ?></p>
-		<br>
+<p><?php ?></p>
+<br>
 
 
+<h3>Componente galeria de fotos</h3>
+<?php
+
+$photoGallery = array(
+
+	'img/prato_1.jpg',
+	'img/prato_2.jpg',
+	'img/prato_3.jpg',
+	'img/prato_4.jpg'
+
+);
+
+?>
+
+<div class="gallery-001">
+
+	<?php foreach ($photoGallery as $photo) { ?>
+
+		<img src="<?php echo $photo; ?>" alt="galeria-fotos">
+
+	<?php } ?>
+
+</div>
 
 
-		<h3>Componente galeria de fotos</h3>
-		<?php
-
-			$photoGallery = array (
-				
-				'img/prato_1.jpg',
-				'img/prato_2.jpg',
-				'img/prato_3.jpg',
-				'img/prato_4.jpg'
-
-			);
-
-		?>
-		
-		<div class="gallery-001">
-
-			<?php foreach ($photoGallery as $photo) { ?>
-
-				<img src="<?php echo $photo; ?>" alt="galeria-fotos">
-
-			<?php } ?>
-
-		</div>
-
-		
+<?php include 'functions/bottom_index.php'; ?>
 
 
-
-		
-
-		
-
-		
-		
-
-
-
-
-
-
-
-
-
-
-		<?php include 'functions/bottom_index.php'; ?>
-
-
-	</body>
-
-</html>
+</body>
