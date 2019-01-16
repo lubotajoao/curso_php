265480607c5016dbf1c9c6c3f8ac5d2c387dec98
Lubota Joao <lubota4@gmail.com>
2019-01-16 14:00:45 -0200

    Curso PHP Basico - [Escopo Das Variaveis]

--- escopo-variaveis.php
+++ escopo-variaveis.php
@@ -1,45 +1,47 @@
-	<?php 
-		include 'header.php';
-		include 'functions/functions.php';
-		$aula_atual = 'escopo-variaveis';
-	?>
+<?php
+include 'header.php';
+include 'functions/functions.php';
+$aula_atual = 'escopo-variaveis';
+?>
+
+<body>
+
+<h2>ESCOPO DAS VARIÁVEIS</h2>
+<hr>
+<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+
+<h3>Escopo Global x Escopo Local</h3>
+
+<?php
+$num = 10;
+
+function print_num(){
+	static $num;
+	$num += 5;
+	echo $num;
+}
+?>
+
+<p>
+	<?php print_num()?>
+</p><br>
+
+<p>
+	<?php print_num()?>
+</p><br>
+
+<p>
+	<?php print_num()?>
+</p><br>
+
+<p>
+	<?php print_num()?>
+</p><br>
 
 
-	<body>
+
+<br><br>
+<?php include 'functions/bottom_index.php'; ?>
 
 
-		<h2>ESCOPO DAS VARIÁVEIS</h2>
-		<hr>
-		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
-
-
-		<h3>Escopo Global x Escopo Local</h3>
-
-			<?php
-				
-
-			?>
-
-			<p>
-			<?php ?>	
-			</p><br>
-
-			<p>
-			<?php ?>	
-			</p><br>
-
-			
-
-			
-
-
-		
-
-
-		<br><br>
-		<?php include 'functions/bottom_index.php'; ?>
-
-
-	</body>
-
-</html>
+</body>
