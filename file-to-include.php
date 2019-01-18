37b35f73183afa843e4beade77b7fa81212c1849
Lubota Joao <lubota4@gmail.com>
2019-01-18 13:57:32 -0200

    Curso PHP Basico - [Include and Require]

--- file-to-include.php
+++ file-to-include.php
@@ -1,13 +1,7 @@
-
-<?php 
-
+<?php
 	$primeiro_nome = 'João';
-
 	function celsiusToFahrenheit($celsius) {
 		$temp = ($celsius * 9 / 5) + 32;
 		return $temp;
 	}
-
-
-
 ?>
--- include-require.php
+++ include-require.php
@@ -3,52 +3,39 @@ include 'header.php';
 $aula_atual = 'include-require';
 ?>
 
-
 <body>
-
-
 <h2>Include e Require</h2>
 <hr>
 <small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
-
-
 <h3>Include</h3>
-
 <?php
+include 'file-to-include.php';
 
+$temperatura = celsiusToFahrenheit(40);
 ?>
 
-
-<p><?php ?></p>
+<p><?php
+	echo $primeiro_nome;
+	echo "<br>" . $temperatura;
+	?></p>
 <br>
-
 
 <h3>Componente galeria de fotos</h3>
 <?php
-
 $photoGallery = array(
-
 	'img/prato_1.jpg',
 	'img/prato_2.jpg',
 	'img/prato_3.jpg',
 	'img/prato_4.jpg'
-
 );
-
 ?>
 
 <div class="gallery-001">
-
 	<?php foreach ($photoGallery as $photo) { ?>
-
 		<img src="<?php echo $photo; ?>" alt="galeria-fotos">
-
 	<?php } ?>
-
 </div>
 
-
 <?php include 'functions/bottom_index.php'; ?>
-
 
 </body>
