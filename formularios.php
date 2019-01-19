b024087ea59ce493dff1ecc8b8e909e56dde2946
Lubota Joao <lubota4@gmail.com>
2019-01-19 02:22:30 -0200

    Curso PHP Basico - [Formulario]

--- formularios.php
+++ formularios.php
@@ -1,61 +1,31 @@
-<?php 
-		include 'header.php';
-		$aula_atual = 'formularios';
-	?>
+<?php
+include 'header.php';
+$aula_atual = 'formularios';
+?>
 
+<body>
+<h2>Formulários</h2>
+<hr>
+<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
 
-	<body>
+<h3>Envie seus dados</h3>
+<form action="valida-formularios.php" method="post">
+	Nome: *
+	<br>
+	<label>
+		<input class="field" name="nome" type="text">
+	</label>
+	<br><br>
 
+	E-mail: *
+	<br>
+	<label>
+		<input class="field" name="email" type="text">
+	</label>
+	<br><br>
 
-		<h2>Formulários</h2>
-		<hr>
-		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+	<input class="submit" name="submit" type="submit"><br>
+</form>
 
-
-		
-
-		<h3>Envie seus dados</h3>
-		
-
-
-
-		<form action="valida-formularios.php" method="post">
-			
-			Nome: *
-			<br>
-			<input type="text" name="nome" class="field">
-			<br><br>
-			
-			E-mail: *
-			<br>
-			<input type="text" name="email" class="field">
-			<br><br>
-
-			<input type="submit" name="submit" class="submit"><br>
-
-		</form>
-
-		
-
-
-
-		
-
-		
-
-		
-		
-
-
-
-
-
-
-
-
-		<?php include 'functions/bottom_index.php'; ?>
-
-
-	</body>
-
-</html>
+<?php include 'functions/bottom_index.php'; ?>
+</body>
